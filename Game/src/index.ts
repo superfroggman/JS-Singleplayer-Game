b9c7f2cb39e4afff2efc48d9903a282eaf0fa999
superfroggman <anrehelenius@gmail.com>
2020-10-05 18:04:55 +0200

    added moving rectangle

--- Game/src/index.ts
+++ Game/src/index.ts
@@ -1,9 +1,20 @@
+let i = -100;
+
 function onLoad() {
   var c = <HTMLCanvasElement>document.getElementById("mainCanvas");
   var ctx = c.getContext("2d");
+
+  ctx?.clearRect(0,0,c.width, c.height)
+  
   if (ctx) {
     ctx.beginPath();
-    ctx.rect(20, 20, 100, 100);
+    ctx.rect(i, 20, 100, 100);
+    ctx.fill();
     ctx.stroke();
   }
+
+  i+= 10;
+  if(i> c.width) i = -100;
+
+  requestAnimationFrame(onLoad);
 }
